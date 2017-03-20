48ed3db94d41b50e6fe3cbdb5c86ec1100c0416a
Clayton Sayer <cbigsby@gmail.com>
2017-03-20 13:38:48 -0700

    Pushing new changes

---
+++ source/utility/matching/Match.cs
@@ -0,0 +1,10 @@
+﻿namespace code.utility.matching
+{
+    public static class Match<ItemToMatch>
+    {
+        public static PropertyMatcher<ItemToMatch, Property> with_attribute<Property>(PropertySelector<ItemToMatch, Property> propertySelector)
+        {
+            return new PropertyMatcher<ItemToMatch, Property>(propertySelector);
+        }
+    }
+}
---
+++ source/utility/matching/PropertySelector.cs
@@ -0,0 +1,4 @@
+﻿namespace code.utility.matching
+{
+    public delegate Property PropertySelector<in Item, out Property>(Item input);
+}
