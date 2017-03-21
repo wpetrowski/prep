539a2da0027c24104dedcbfd13b5a8183e729e8d
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2017-03-21 10:38:12 -0600

    Updated

--- source/utility/matching/Range.cs
+++ source/utility/matching/Range.cs
@@ -1,28 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace code.utility.matching
 {
-
   public static class Range
   {
     public static Criteria<Value> after<Value>(Value value, bool inclusive) where Value : IComparable<Value>
     {
       return x => inclusive ? x.CompareTo(value) >= 0 : x.CompareTo(value) > 0;
     }
-    
+
     public static Criteria<Value> before<Value>(Value value, bool inclusive) where Value : IComparable<Value>
     {
       return x => inclusive ? x.CompareTo(value) <= 0 : x.CompareTo(value) < 0;
     }
-    
-    public static Criteria<Value> between<Value>(Value start, bool inclusiveStart, Value end, bool inclusiveEnd) where Value : IComparable<Value>
+
+    public static Criteria<Value> between<Value>(Value start, bool inclusive_start, Value end, bool inclusive_end)
+      where Value : IComparable<Value>
     {
-      //return x => inclusive ? x.CompareTo(start) >= 0 && x.CompareTo(end) <= 0 : x.CompareTo(start) > 0 && x.CompareTo(end) < 0;
-      return before(end, inclusiveEnd).and(after(start, inclusiveStart));
+      return before(end, inclusive_end).and(after(start, inclusive_start));
     }
+
   }
+
 }
