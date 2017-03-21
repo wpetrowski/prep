4a3b072f7b55d7c030a9b480d0f2408fb6c55e3c
Wesley Petrowski <wesley@petrowski.ca>
2017-03-21 13:58:11 -0500

    Pushing new changes

--- source/prep/movies/Sort.cs
+++ source/prep/movies/Sort.cs
@@ -16,5 +16,22 @@ namespace code.prep.movies
       return (a, b) => accessor(a).CompareTo(accessor(b));
     }
 
+    public static ICompareTwoItems<Item> by<Property>(IGetTheValueOfAProperty<Item, Property> accessor, params Property[] sort_order)
+    {
+      return (a, b) => Array.IndexOf(sort_order, accessor(a)) - Array.IndexOf(sort_order, accessor(b));
+    }
+  }
+
+  public static class SortExtensions
+  {
+    public static ICompareTwoItems<Item> then_by<Item, Property>(this ICompareTwoItems<Item> previous_comparer, IGetTheValueOfAProperty<Item, Property> accessor) where Property : IComparable<Property>
+    {
+      return (a, b) =>
+      {
+        int previous_result = previous_comparer(a, b);
+        return previous_result == 0 ? Sort<Item>.by(accessor)(a, b) : previous_result;
+      };
+    }
+
   }
 }
