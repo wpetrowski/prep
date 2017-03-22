c0ba7236d05a7f7b7406866b6b8d26c815a01fc4
Lucas Cramer <LucasC@iqmetrix.com>
2017-03-22 11:37:57 -0700

    Pushing new changes

--- source/web/CommandRegistry.cs
+++ source/web/CommandRegistry.cs
@@ -7,15 +7,20 @@ namespace code.web
   public class CommandRegistry :IFindACommandThatCanHandleARequest
   {
       private readonly IEnumerable<IHandleOneWebRequest> commands;
+      private ICreateAMissingCommandWhenOneCantBeFound missingCommandCreator;
 
-      public CommandRegistry(IEnumerable<IHandleOneWebRequest> commands)
+      public CommandRegistry(IEnumerable<IHandleOneWebRequest> commands,
+                             ICreateAMissingCommandWhenOneCantBeFound missingCommandCreator)
       {
           this.commands = commands;
+          this.missingCommandCreator = missingCommandCreator;
       }
 
      public IHandleOneWebRequest get_command_that_can_handle(IProvideDetailsAboutAWebRequest request)
      {
-         return commands.First(command => command.can_process(request));
+         var matchingCommand = commands.FirstOrDefault(command => command.can_process(request));
+
+         return matchingCommand ?? missingCommandCreator(request);
      }
   }
 }
