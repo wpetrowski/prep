35abc5bbb43dd88e8dfae5ae4aa3eb0e64c64101
Wesley Petrowski <wesley@petrowski.ca>
2017-03-23 09:51:44 -0500

    Pushing new changes

--- source/utility.iteration/VisitorExtensions.cs
+++ source/utility.iteration/VisitorExtensions.cs
@@ -9,7 +9,8 @@ namespace code.utility.iteration
     public static Result get_result_of_processing_all_with<Element, Result>(this IEnumerable<Element> items,
       IProcessAndReturnAValue<Element, Result> visitor)
     {
-      throw new NotImplementedException(); 
+      items.each_for_all(visitor.process);
+      return visitor.get_result();
     }
 
     public static void process_all_using<Element>(this IEnumerable<Element> items, IProcessAn<Element> visitor)
