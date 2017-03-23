14721aace50be9d97d2b589de916a3fec20bfc35
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2017-03-23 08:44:11 -0600

    Add new test

--- source/utility.iteration/VisitorSpikes.cs
+++ source/utility.iteration/VisitorSpikes.cs
@@ -1,7 +1,7 @@
 ﻿using System.Linq;
-using Machine.Specifications;
 using developwithpassion.specifications.assertions.core;
 using developwithpassion.specifications.assertions.interactions;
+using Machine.Specifications;
 using Rhino.Mocks;
 using spec = developwithpassion.specifications.use_engine<Machine.Fakes.Adapters.Rhinomocks.RhinoFakeEngine>;
 
@@ -11,7 +11,6 @@ namespace code.utility.iteration
   {
     public abstract class concern : spec.observe
     {
-
     }
 
     public class when_processing_all_of_the_items_in_a_iterator : concern
@@ -28,8 +27,24 @@ namespace code.utility.iteration
         visitor.should().received(x => x.process(Arg<int>.Is.Anything)).Times(10);
 
       static IProcessAn<int> visitor;
-        
-        
+    }
+
+    public class when_getting_the_result_of_processing_all_items_in_an_iterator_with_a_visitor : concern
+    {
+      Establish c = () =>
+      {
+        visitor = fake.an<IProcessAn<int>>();
+      };
+
+      Because b = () =>
+        Enumerable.Range(1, 10).process_all_using(visitor);
+
+      It returns_the_result_of_the_visitor = () =>
+        result.ShouldEqual(expected_result);
+
+      static IProcessAn<int> visitor;
+      static int result;
+      static int expected_result;
     }
   }
 }
